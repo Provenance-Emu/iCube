046a57e8a02a29278925d010572edeef920dc0c1
Jordan Woyak <jordan.woyak@gmail.com>
2025-06-29 20:23:41 -0500

    CoreTiming: Fix unsafe usage of m_globals.global_timer in ScheduleEvent from non-CPU thread.
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>
    
    # Conflicts:
    #	Source/Core/Core/CoreTiming.h

--- Source/Core/Core/CoreTiming.h
+++ Source/Core/Core/CoreTiming.h
@@ -164,7 +164,7 @@ public:
   bool GetVISkip() const;                           // Used By VideoInterface
 
   float GetOverclock() const;
-  
+
   bool UseSyncOnSkipIdle() const;
 
 private:
@@ -184,7 +184,10 @@ private:
   std::vector<Event> m_event_queue;
   u64 m_event_fifo_id = 0;
   std::mutex m_ts_write_lock;
-  Common::SPSCQueue<Event, false> m_ts_queue;
+
+  // Event objects created from other threads.
+  // The time value of each Event here is a cycles_into_future value.
+  Common::SPSCQueue<Event> m_ts_queue;
 
   float m_last_oc_factor = 0.0f;
 
