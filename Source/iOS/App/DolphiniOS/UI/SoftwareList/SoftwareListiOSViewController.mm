d6e4997e143725fe025c107d2386bca44bd7c191
Joseph Mattiello <git@joemattiello.com>
2025-07-15 16:11:47 -0400

    fix file associations
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.mm
+++ Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.mm
@@ -36,26 +36,26 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
 
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
-  
+
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveImportFileFinishedNotification) name:DOLImportFileFinishedNotification object:nil];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
   [super viewWillDisappear:animated];
-  
+
   [[NSNotificationCenter defaultCenter] removeObserver:self name:DOLImportFileFinishedNotification object:nil];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
-  
+
   if (_openedUrl != nil) {
     [_openedUrl stopAccessingSecurityScopedResource];
     _openedUrl = nil;
   }
-  
+
   NSArray<UIMenuElement*>* wiiActions;
-  
+
   UIMenuElement* wiiNandElement = [UIMenu menuWithTitle:DOLCoreLocalizedString(@"Manage NAND") children:@[
     [UIAction actionWithTitle:DOLCoreLocalizedString(@"Import BootMii NAND Backup...") image:[UIImage systemImageNamed:@"internaldrive"] identifier:nil handler:^(UIAction*) {
       [self openDocumentPickerWithContentTypes:@[
@@ -63,27 +63,27 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
       ] pickerType:DOLSoftwareListDocumentPickerTypeImportNAND];
     }]
   ]];
-  
+
   // Get the system menu TMD
   IOS::HLE::Kernel ios;
   const auto tmd = ios.GetESCore().FindInstalledTMD(Titles::SYSTEM_MENU);
-  
+
   if (tmd.IsValid()) {
     NSString* loadFormat;
-    
+
     if (tmd.IsvWii()) {
       loadFormat = DOLCoreLocalizedStringWithArgs(@"Load vWii System Menu %1", @"@");
     } else {
       loadFormat = DOLCoreLocalizedStringWithArgs(@"Load Wii System Menu %1", @"@");
     }
-    
+
     std::string version = DiscIO::GetSysMenuVersionString(tmd.GetTitleVersion(), tmd.IsvWii());
-    
+
     wiiActions = @[
       [UIAction actionWithTitle:[NSString stringWithFormat:loadFormat, CppToFoundationString(version)] image:[UIImage systemImageNamed:@"tray.and.arrow.down"] identifier:nil handler:^(UIAction*) {
         self->_bootParameter = [[EmulationBootParameter alloc] init];
         self->_bootParameter.bootType = EmulationBootTypeSystemMenu;
-        
+
         [self performSegueWithIdentifier:@"emulation" sender:nil];
       }],
       wiiNandElement,
@@ -110,25 +110,25 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
       ]
     ]];
   }
-  
+
   NSMutableArray<UIMenuElement*>* iplActions = [[NSMutableArray alloc] init];
-  
+
   void(^addIPLAction)(DiscIO::Region, NSString*, std::string) = ^(DiscIO::Region region, NSString* regionName, std::string regionDir) {
     UIAction* iplAction = [UIAction actionWithTitle:DOLCoreLocalizedString(regionName) image:nil identifier:nil handler:^(UIAction*) {
       [self loadGameCubeIPLForRegion:region];
     }];
-    
+
     if (!File::Exists(Config::GetBootROMPath(regionDir))) {
       [iplAction setAttributes:UIMenuElementAttributesDisabled];
     }
-    
+
     [iplActions addObject:iplAction];
   };
-  
+
   addIPLAction(DiscIO::Region::NTSC_J, @"NTSC-J", JAP_DIR);
   addIPLAction(DiscIO::Region::NTSC_U, @"NTSC-U", USA_DIR);
   addIPLAction(DiscIO::Region::PAL, @"PAL", EUR_DIR);
-  
+
   self.navigationItem.leftBarButtonItem.menu = [UIMenu menuWithChildren:@[
     [UIAction actionWithTitle:DOLCoreLocalizedString(@"Open") image:[UIImage systemImageNamed:@"externaldrive"] identifier:nil handler:^(UIAction*) {
       [self openDocumentPickerWithSoftwareContentTypesAndPickerType:DOLSoftwareListDocumentPickerTypeOpenExternal];
@@ -144,9 +144,15 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
   NSArray<UTType*>* types = @[
     [UTType exportedTypeWithIdentifier:@"me.oatmealdome.dolphinios.generic-software"],
     [UTType exportedTypeWithIdentifier:@"me.oatmealdome.dolphinios.gamecube-software"],
-    [UTType exportedTypeWithIdentifier:@"me.oatmealdome.dolphinios.wii-software"]
+    [UTType exportedTypeWithIdentifier:@"me.oatmealdome.dolphinios.wii-software"],
+    [UTType typeWithIdentifier:@"public.iso-image"],
+    [UTType typeWithIdentifier:@"me.oatmealdome.dolphinios.rvz-image"],
+    [UTType typeWithIdentifier:@"me.oatmealdome.dolphinios.dol-executable"],
+    [UTType typeWithIdentifier:@"me.oatmealdome.dolphinios.elf-executable"],
+    [UTType typeWithIdentifier:@"public.archive"],
+    [UTType typeWithIdentifier:@"public.data"]
   ];
-  
+
   [self openDocumentPickerWithContentTypes:types pickerType:pickerType];
 }
 
@@ -155,9 +161,9 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
   pickerController.delegate = self;
   pickerController.modalPresentationStyle = UIModalPresentationPageSheet;
   pickerController.allowsMultipleSelection = false;
-  
+
   _pickerType = pickerType;
-  
+
   [self presentViewController:pickerController animated:true completion:nil];
 }
 
@@ -168,49 +174,49 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
 - (void)documentPicker:(UIDocumentPickerViewController*)controller didPickDocumentsAtURLs:(NSArray<NSURL*>*)urls {
   void (^showError)(NSString*) = ^(NSString* error) {
     UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:error preferredStyle:UIAlertControllerStyleAlert];
-    
+
     [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
       handler:nil]];
-    
+
     [self presentViewController:errorAlert animated:true completion:nil];
   };
-  
+
   if (_pickerType == DOLSoftwareListDocumentPickerTypeImportSoftware) {
     [[ImportFileManager shared] importFileAtUrl:urls[0]];
   } else if (_pickerType == DOLSoftwareListDocumentPickerTypeOpenExternal) {
     NSURL* url = urls[0];
-    
+
     if (![url startAccessingSecurityScopedResource]) {
       showError(@"Failed to start accessing security scoped resource.");
       return;
     }
-    
+
     _openedUrl = url;
-    
+
     NSString* sourcePath = [_openedUrl path];
-    
+
     GameFilePtrWrapper* gameFileWrapper = [[GameFilePtrWrapper alloc] init];
     gameFileWrapper.gameFile = std::make_shared<UICommon::GameFile>(FoundationToCppString(sourcePath));
-    
+
     if (!gameFileWrapper.gameFile->IsValid()) {
       [_openedUrl stopAccessingSecurityScopedResource];
-      
+
       showError(@"File is invalid.");
-      
+
       return;
     }
-    
+
     [self loadGameFile:gameFileWrapper];
   } else if (_pickerType == DOLSoftwareListDocumentPickerTypeImportNAND) {
     NSURL* url = urls[0];
-    
+
     if (![url startAccessingSecurityScopedResource]) {
       showError(@"Failed to start accessing security scoped resource.");
       return;
     }
-    
+
     UIAlertController* waitAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Importing NAND backup") message:nil preferredStyle:UIAlertControllerStyleAlert];
-    
+
     [self presentViewController:waitAlert animated:true completion:^{
       dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
         DiscIO::NANDImporter().ImportNANDBin(FoundationToCppString([url path]), [] {
@@ -220,9 +226,9 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
           PanicAlertFmtT("The decryption keys need to be appended to the NAND backup file.");
           return "";
         });
-        
+
         [url stopAccessingSecurityScopedResource];
-        
+
         dispatch_async(dispatch_get_main_queue(), ^{
           [waitAlert dismissViewControllerAnimated:true completion:nil];
         });
@@ -234,35 +240,35 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
 - (UIContextMenuConfiguration*)collectionView:(UICollectionView*)collectionView contextMenuConfigurationForItemAtIndexPath:(NSIndexPath*)indexPath point:(CGPoint)point {
   return [UIContextMenuConfiguration configurationWithIdentifier:nil previewProvider:nil actionProvider:^(NSArray<UIMenuElement*>*) {
     GameFilePtrWrapper* gameFileWrapper = [self->_gameFiles objectAtIndex:indexPath.row];
-    
+
     NSMutableArray<UIAction*>* actions = [[NSMutableArray alloc] init];
-    
+
     [actions addObject:[UIAction actionWithTitle:DOLCoreLocalizedString(@"Properties") image:[UIImage systemImageNamed:@"square.and.pencil"] identifier:nil handler:^(UIAction*) {
       self->_selectedFile = gameFileWrapper;
-      
+
       [self performSegueWithIdentifier:@"properties" sender:nil];
     }]];
-    
+
     UIAction* deleteAction = [UIAction actionWithTitle:DOLCoreLocalizedString(@"Delete") image:[UIImage systemImageNamed:@"trash"] identifier:nil handler:^(UIAction*) {
       UIAlertController* confirmAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Confirm") message:DOLCoreLocalizedString(@"Are you sure you want to delete this file?") preferredStyle:UIAlertControllerStyleAlert];
-        
+
       [confirmAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"No") style:UIAlertActionStyleDefault handler:nil]];
-      
+
       [confirmAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"Yes") style:UIAlertActionStyleDestructive handler:^(UIAlertAction*) {
         if (File::Delete(gameFileWrapper.gameFile->GetFilePath())) {
           [self reloadGameFiles];
         }
       }]];
-      
+
       [self presentViewController:confirmAlert animated:true completion:nil];
     }];
-    
+
     [deleteAction setAttributes:UIMenuElementAttributesDestructive];
-    
+
     [actions addObject:deleteAction];
-    
+
     NSString* gameName = CppToFoundationString(gameFileWrapper.gameFile->GetName(UICommon::GameFile::Variant::LongAndPossiblyCustom));
-    
+
     return [UIMenu menuWithTitle:gameName children:[actions copy]];
   }];
 }
