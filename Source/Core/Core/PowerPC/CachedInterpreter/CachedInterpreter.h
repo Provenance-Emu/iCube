3ec2ebe041ca3c35ae38bde27a65ecbabe972d86
Joseph Mattiello <git@joemattiello.com>
2025-08-19 01:48:02 -0400

    kMaxOps 32
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Source/Core/Core/PowerPC/CachedInterpreter/CachedInterpreter.h
+++ Source/Core/Core/PowerPC/CachedInterpreter/CachedInterpreter.h
@@ -214,7 +214,7 @@ struct MicroOp
 struct CachedInterpreter::ExecuteMicroOpsOperands
 {
   // Embedded small array keeps lifetime simple and avoids heap allocs
-  static constexpr u32 kMaxOps = 16;
+  static constexpr u32 kMaxOps = 32;
   u32 count;
   MicroOp ops[kMaxOps];
   u32 current_pc;
