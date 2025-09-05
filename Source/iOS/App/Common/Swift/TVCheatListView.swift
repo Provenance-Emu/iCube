44bcc968f2dac472e6fa3328ca2e4dfb2d73091e
Joseph Mattiello <git@joemattiello.com>
2025-09-04 20:02:47 -0400

    cheats global on / off option
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Source/iOS/App/Common/Swift/TVCheatListView.swift
+++ Source/iOS/App/Common/Swift/TVCheatListView.swift
@@ -8,6 +8,11 @@ struct TVCheatListView: View {
     @State private var ar: [TVActionReplayCodeInfo] = []
     @Environment(\.dismiss) private var dismissEnv
 
+    @State private var cheatsEnabledGlobal: Bool = false
+    @State private var showEnableCheatsPrompt: Bool = false
+    @State private var pendingIsGecko: Bool? = nil
+    @State private var pendingIndex: Int? = nil
+
     var body: some View {
         NavigationStack {
             VStack {
@@ -19,15 +24,27 @@ struct TVCheatListView: View {
                 .padding()
 
                 List {
+                    Section {
+                        Toggle(L("Enable Cheats"), isOn: Binding(get: { cheatsEnabledGlobal }, set: { newValue in
+                            DOLConfigBridge.setMainEnableCheats(newValue)
+                            cheatsEnabledGlobal = newValue
+                        }))
+                    }
                     if tab == .gecko {
                         ForEach(gecko.indices, id: \.self) { i in
                             HStack {
                                 Text(gecko[i].name)
                                 Spacer()
                                 Toggle("", isOn: Binding(get: { gecko[i].enabled }, set: { newValue in
-                                    gecko[i].enabled = newValue
-                                    _ = TVCheatsBridge.setGeckoCodeEnabled(newValue, at: i, forGameId: item.gameID, revision: item.revision)
-                                    reload()
+                                    if newValue && !cheatsEnabledGlobal {
+                                        pendingIsGecko = true
+                                        pendingIndex = i
+                                        showEnableCheatsPrompt = true
+                                    } else {
+                                        gecko[i].enabled = newValue
+                                        _ = TVCheatsBridge.setGeckoCodeEnabled(newValue, at: i, forGameId: item.gameID, revision: item.revision)
+                                        reload()
+                                    }
                                 }))
                                 .labelsHidden()
                             }
@@ -38,9 +55,15 @@ struct TVCheatListView: View {
                                 Text(ar[i].name)
                                 Spacer()
                                 Toggle("", isOn: Binding(get: { ar[i].enabled }, set: { newValue in
-                                    ar[i].enabled = newValue
-                                    _ = TVCheatsBridge.setActionReplayCodeEnabled(newValue, at: i, forGameId: item.gameID, revision: item.revision)
-                                    reload()
+                                    if newValue && !cheatsEnabledGlobal {
+                                        pendingIsGecko = false
+                                        pendingIndex = i
+                                        showEnableCheatsPrompt = true
+                                    } else {
+                                        ar[i].enabled = newValue
+                                        _ = TVCheatsBridge.setActionReplayCodeEnabled(newValue, at: i, forGameId: item.gameID, revision: item.revision)
+                                        reload()
+                                    }
                                 }))
                                 .labelsHidden()
                             }
@@ -52,7 +75,34 @@ struct TVCheatListView: View {
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) { Button("Done") { dismissEnv() } }
             }
-            .onAppear(perform: reload)
+            .onAppear {
+                cheatsEnabledGlobal = DOLConfigBridge.mainEnableCheats()
+                reload()
+            }
+        }
+        .alert(L("Enable Cheats?"), isPresented: $showEnableCheatsPrompt) {
+            Button(L("Turn On Cheats")) {
+                DOLConfigBridge.setMainEnableCheats(true)
+                cheatsEnabledGlobal = true
+                if let idx = pendingIndex, let isGecko = pendingIsGecko {
+                    if isGecko {
+                        gecko[idx].enabled = true
+                        _ = TVCheatsBridge.setGeckoCodeEnabled(true, at: idx, forGameId: item.gameID, revision: item.revision)
+                    } else {
+                        ar[idx].enabled = true
+                        _ = TVCheatsBridge.setActionReplayCodeEnabled(true, at: idx, forGameId: item.gameID, revision: item.revision)
+                    }
+                    pendingIndex = nil
+                    pendingIsGecko = nil
+                    reload()
+                }
+            }
+            Button(L("Cancel"), role: .cancel) {
+                pendingIndex = nil
+                pendingIsGecko = nil
+            }
+        } message: {
+            Text(L("Cheats can affect performance and stability. Enable global cheats to apply this code?"))
         }
     }
 
