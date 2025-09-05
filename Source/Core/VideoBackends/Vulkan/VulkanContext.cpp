f86a5ea452545aeaad852e4c6654823a581fe2a6
Joseph Mattiello <git@joemattiello.com>
2025-09-05 16:55:40 -0400

    On MoltenVK, prefer inverted depth to avoid depth compare mismatches on some devices
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Source/Core/VideoBackends/Vulkan/VulkanContext.cpp
+++ Source/Core/VideoBackends/Vulkan/VulkanContext.cpp
@@ -553,6 +553,10 @@ void VulkanContext::PopulateBackendInfoFeatures(VideoConfig* config, VkPhysicalD
   if (DriverDetails::HasBug(DriverDetails::BUG_BROKEN_REVERSED_DEPTH_RANGE))
     config->backend_info.bSupportsReversedDepthRange = false;
 
+  // On MoltenVK, prefer inverted depth to avoid depth compare mismatches on some devices
+  if (info.driverID == VK_DRIVER_ID_MOLTENVK)
+    config->backend_info.bSupportsReversedDepthRange = false;
+
   // Dynamic sampler indexing locks up Intel GPUs on MoltenVK/Metal
   if (DriverDetails::HasBug(DriverDetails::BUG_BROKEN_DYNAMIC_SAMPLER_INDEXING))
     config->backend_info.bSupportsDynamicSamplerIndexing = false;
