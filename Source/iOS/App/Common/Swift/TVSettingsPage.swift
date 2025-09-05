240a255950c7b6bd5cc88c55a67e3dabb733adc0
Joseph Mattiello <git@joemattiello.com>
2025-09-04 20:29:49 -0400

    remove close on settings for ios
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Source/iOS/App/Common/Swift/TVSettingsPage.swift
+++ Source/iOS/App/Common/Swift/TVSettingsPage.swift
@@ -9,12 +9,14 @@ internal struct TVSettingsPage: View {
     @Environment(\.dismiss) private var dismiss
     var body: some View {
         SettingsRootView()
+      #if os(tvOS)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Close") { dismiss() }
                         .buttonStyle(.bordered)
                 }
             }
+      #endif
             .background(Color.black)
     }
 }
